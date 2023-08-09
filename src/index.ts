3cecf79c9d699f55547adfe1344a3c35914ba646
Jedikrigeren <59309403+Jedikrigeren@users.noreply.github.com>
2023-08-09 11:55:04 +0200

    added a cron log

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'https://deno.land/std@0.195.0/dotenv/load.ts'
 import { checkEnvs } from './utils/handleCheckingEnvs.ts'
 import { iterateDeliveryNotes } from './utils/handleIterateDeliveries.ts'
 import { logoutSap } from './fragt-api-wrapper/POST-logout.ts'
+import { cron, daily, monthly, weekly } from 'https://deno.land/x/deno_cron@v1.0.0/cron.ts'
 
 async function main() {
   // Github repo for running deno on Pi (Seemingly only works in the terminal you run the curl script and export in, but it works
@@ -11,11 +12,17 @@ async function main() {
   if (result.type == 'error') {
     console.log(result.error)
   } else {
-    const timestamp = new Date(new Date().getTime()).toLocaleString()
-    console.log(timestamp + ': Running the pre Cron job')
+    console.log(new Date(new Date().getTime()).toLocaleString() + ': Running the pre Cron job')
     await iterateDeliveryNotes()
-    console.log(timestamp + ': Finished the initial run')
+    console.log(new Date(new Date().getTime()).toLocaleString() + ': Finished the initial run')
     await logoutSap()
+
+    cron('0 0 5 * * *', async () => {
+      console.log(new Date(new Date().getTime()).toLocaleString() + ': Running the daily Cron job')
+      await iterateDeliveryNotes()
+      console.log(new Date(new Date().getTime()).toLocaleString() + ': Finished the daily run')
+      await logoutSap()
+    })
   }
 }
 
--- src/utils/handleIterateDeliveries.ts
+++ src/utils/handleIterateDeliveries.ts
@@ -29,7 +29,7 @@ export async function iterateDeliveryNotes() {
       continue
     }
     const validationResponse = await validateAddress(order.AddressExtension, order.CardCode, order.DocNum)
-    sleep(1000 * 5) // Sleep for 5 seconds to let the address validation finish
+    await sleep(1000 * 5) // Sleep for 5 seconds to let the address validation finish
     if (validationResponse) {
       setAddressValidation(order.DocEntry, order.DocNum, validationResponse)
       continue
