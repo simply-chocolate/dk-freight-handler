fb77d3df188dc3b42c2af1c6a7995a6ef695e7e3
Jedikrigeren <hasselsteen95@live.dk>
2024-05-21 21:07:15 +0200

    tried adding a teams message

--- src/indexBookFreight.ts
+++ src/indexBookFreight.ts
@@ -1,12 +1,9 @@
 import 'https://deno.land/std@0.195.0/dotenv/load.ts'
-import { cron } from 'https://deno.land/x/deno_cron@v1.0.0/cron.ts'
 import { logoutSap } from './sap-api-wrapper/POST-logout.ts'
-import { handleCheckValidatedBusinessPartners } from './utils/handleCheckValidatedBusinessPartners.ts'
+import { sendTeamsMessage } from "./teams_notifier/SEND-teamsMessage.ts"
 import { checkEnvs } from './utils/handleCheckingEnvs.ts'
 import { iterateDeliveryNotes } from './utils/handleIterateDeliveries.ts'
 import { iterateStockTransfers } from './utils/handleIterateStockTransfers.ts'
-import { validateBusinessPartners } from './utils/handleValidateBusinessPartners.ts'
-import { validateOpenOrders } from './utils/handleValidateOpenOrders.ts'
 
 async function mainBF() {
   // Github repo for running deno on Pi (Seemingly only works in the terminal you run the curl script and export in, but it works
@@ -20,6 +17,7 @@ async function mainBF() {
     // BOOKING FREIGHT AND PRINTING LABELS
     try {
       console.log('Starting Book Freight process')
+      await sendTeamsMessage('Starting book freight process')
       await iterateDeliveryNotes()
       await iterateStockTransfers()
       await logoutSap()
